995c68e7d16c1d529ba719b09a657383755f47dc
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-06-06 21:51:35 +0500

    navbar done
    navbar done

---
+++ netflix/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+function App() {
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
