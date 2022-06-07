f567ea7f3f4fc7116582a171802df77674845abb
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-06-08 00:15:12 +0500

    gradients completed
    done

--- scrollbar/src/App.tsx
+++ scrollbar/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
 	return (
-		<div className='flex justify-center items-center min-h-[100vh] '>
+		<div className='flex justify-center items-center min-h-[100vh] bg-gradient-to-br from-white to-black underline hover:from-pink-500 hover:to-indigo-900'>
 			<h1 className='coloredTexth1'>Mishaal khan</h1>
 		</div>
 	);
