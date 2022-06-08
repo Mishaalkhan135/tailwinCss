b3dd09f8834401b18e671264f3a22fea4b780188
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-06-08 22:08:19 +0500

    video player added
    video player added

--- scrollbar/src/App.tsx
+++ scrollbar/src/App.tsx
@@ -4,14 +4,16 @@ import Body from "./components/body";
 import Cards from "./components/cards";
 import FileUpload from "./components/fileUpload";
 import Signup from "./components/signUp";
+import Video from "./components/video";
 function App() {
 	return (
 		<div>
 			{/* <Header />
 			<Body />
 			<Cards />
-			<FileUpload /> */}
-			<Signup />
+			<FileUpload /> 
+			<Signup />*/}
+			<Video />
 		</div>
 	);
 }
---
+++ scrollbar/src/components/video.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+const video = () => {
+	return (
+		<div className='flex justify-center items-center bg-black'>
+			<ReactPlayer
+				width='400px'
+				height='280px'
+				controls
+				url='https://www.youtube.com/watch?v=fC9CwJzmb_M'
+			/>
+		</div>
+	);
+};
+
+export default video;
