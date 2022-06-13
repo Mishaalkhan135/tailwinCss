17bc7d33fc62ffe8bb737e7860884d5ead92c3d0
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-06-13 22:48:15 +0500

    AppBar , Navbar ,images and cards
    APPBar , Navbar ,images and cards

--- ecommerce-store/src/App.js
+++ ecommerce-store/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 // // import Cart from "./pages/Cart";
 // import Login from "./pages/Login";
 // import Category from "./pages/CategoryPage";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 // import ProductPage from "./pages/ProductPage";
-import Register from "./pages/Register";
-
+// import Register from "./pages/Register";
+import Drawer from "./components/Drawer";
 function App() {
 	return (
 		<div>
-			<Home />
+			<Drawer />
 		</div>
 	);
 }
